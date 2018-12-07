703b4eee40e398ed837c144b7c9d9175df38825d
Ögeday Sunar <ogeday.sunar@isik.edu.tr>
2018-12-07 14:27:09 +0300

    Login Update
    I added the changes that elif teacher said.

--- Login/LoginActivity.java
+++ Login/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.ogedaysunar.se301;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -24,8 +25,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            Intent intocan = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intocan);
+        } else {
+            // No user is signed in
+        }
+
+
         Button clickButton = (Button) findViewById(R.id.login);
         final EditText edt1 = (EditText) findViewById(R.id.email);
         final EditText edt2 = (EditText) findViewById(R.id.password);
@@ -37,25 +47,34 @@ public class LoginActivity extends AppCompatActivity {
 
                 if(TextUtils.isEmpty(email)){
                     Toast.makeText(getApplicationContext(),"Please enter your E-mail",Toast.LENGTH_SHORT).show();
-                    return;
+                    
                 }
-                if(TextUtils.isEmpty(parola)){
+                else if(TextUtils.isEmpty(parola)){
                     Toast.makeText(getApplicationContext(),"Please enter your Password",Toast.LENGTH_SHORT).show();
                 }
-                mAuth.signInWithEmailAndPassword(email, parola)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Your Login is Success",Toast.LENGTH_SHORT).show();
-                                }
-                                else {
-                                    Toast.makeText(getApplicationContext(), "Login Failed",Toast.LENGTH_SHORT).show();
-                                }
 
-                                // ...
-                            }
-                        });
+                else if(parola.length() < 5){
+                    Toast.makeText(getApplicationContext(),"Your password is wrong",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    mAuth.signInWithEmailAndPassword(email, parola)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Your Login is Success",Toast.LENGTH_SHORT).show();
+                                        Intent intocan = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intocan);
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), "Login Failed",Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+
             }
         });
     }
---
+++ Login/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ogedaysunar.se301;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        Button clickButton = (Button) findViewById(R.id.logoutButton);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intocan = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intocan);
+            }
+        });
+
+    }
+
+}
