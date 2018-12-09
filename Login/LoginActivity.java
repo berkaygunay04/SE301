da60c42a00f16d74011d3ed5560f6aabf072d262
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2018-12-09 20:34:44 +0300

    Login
    Login Değişiklikler eklendi.

--- Login/LoginActivity.java
+++ Login/LoginActivity.java
@@ -20,13 +20,18 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    TextView a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+        final FirebaseUser user = mAuth.getCurrentUser();
+        a=(TextView)findViewById(R.id.registertext);
+        SpannableString content = new SpannableString("If you haven't account Register");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        a.setText(content);
         if (user != null) {
             // User is signed in
             Intent intocan = new Intent(LoginActivity.this, MainActivity.class);
@@ -48,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
                 String email = edt1.getText().toString();
                 String parola = edt2.getText().toString();
 
-                if(TextUtils.isEmpty(email)){
+                if(TextUtils.isEmpty(email) || !isEmailValid(email)){
                     Toast.makeText(getApplicationContext(),"Please enter your E-mail",Toast.LENGTH_SHORT).show();
 
                 }
@@ -66,7 +71,7 @@ public class LoginActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Toast.makeText(getApplicationContext(), "Your Login is Success",Toast.LENGTH_SHORT).show();
-                                        Intent intocan = new Intent(LoginActivity.this, MainActivity.class);
+                                        Intent intocan = new Intent(LoginActivity.this, Main4Activity.class);
                                         startActivity(intocan);
                                     }
                                     else {
@@ -89,6 +94,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
+ public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public void openActivity15(View view) {
+        Intent register = new Intent(Main3Activity.this, MainActivity.class);
+        startActivity(register);
+    }
 
 }
