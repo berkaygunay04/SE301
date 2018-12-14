488e656b9a78fb034ab56d202ee0fd8c316bcaf8
toguzkurt <44292703+toguzkurt@users.noreply.github.com>
2018-12-14 19:07:33 +0300

    ElectionObject.java
    Election adapter veri içerikleri.

---
+++ Election/ElectionObject.java
@@ -0,0 +1,13 @@
+package voted.tnc.ivoted;
+public class ElectionObject {//Election Objesi
+    String name;
+    String des;
+    int image;
+    String id;
+    ElectionObject (String name, String des,int image,String id){
+        this.name=name;
+        this.des=des;
+        this.image=image;
+        this.id=id;
+    }
+}
