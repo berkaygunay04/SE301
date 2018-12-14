acde4bfa2dc3603e8436595ae873a7f384490f45
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2018-12-15 02:52:36 +0300

    Menu
    Menu Java kaynak kodları

--- Menu/Main5Activity.java
+++ Menu/Main5Activity.java
@@ -39,47 +39,63 @@ public class Main5Activity extends ListActivity {
     private ArrayList<Model> generateData(){
         models = new ArrayList<Model>();
         models.add(new Model("Profile Settings"));
-        models.add(new Model(R.drawable.ic_menu_black_24dp,"Profil Bilgileri","1"));
+        models.add(new Model(R.drawable.ic_menu_black_24dp,"Profile Settings","1"));
         models.add(new Model(R.drawable.ic_menu_black_24dp,"My Votes","2"));
         models.add(new Model(R.drawable.ic_menu_black_24dp,"Applied Elections","12"));
         models.add(new Model("About"));
         models.add(new Model(R.drawable.ic_menu_black_24dp,"Web","12"));
-        models.add(new Model(R.drawable.ic_menu_black_24dp,"Kullanıcı Sözleşmesi","12"));
-        models.add(new Model(R.drawable.ic_menu_black_24dp,"İletişim","12"));
-        models.add(new Model(R.drawable.ic_menu_black_24dp,"Geliştiriciler","12"));
-        models.add(new Model(R.drawable.ic_done_white_48dp,"                     Çıkış Yap","12"));
+        models.add(new Model(R.drawable.ic_menu_black_24dp,"User Agreement","12"));
+        models.add(new Model(R.drawable.ic_menu_black_24dp,"Contact","12"));
+        models.add(new Model(R.drawable.ic_menu_black_24dp,"Developers","12"));
+        models.add(new Model(R.drawable.ic_done_white_48dp,"                     Log Out","12"));
         return models;
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        if(models.get(position).getTitle().equals("                     Çıkış Yap")){
+        if(models.get(position).getTitle().equals("                     Log Out")){
 
         }else{
             Toast.makeText(this, "Seçtiniz: "+models.get(position).getTitle()+ " " , Toast.LENGTH_LONG).show();
         }
 
-        if(models.get(position).getTitle()=="                     Çıkış Yap"){
+        if(models.get(position).getTitle()=="                     Log Out"){
             AlertDialog.Builder builder1=new AlertDialog.Builder(Main5Activity.this);
-            builder1.setTitle("Çıkış!");
+            builder1.setTitle("Log Out!");
             builder1.setMessage("Çıkış yapmak istediğinize emin misiniz?");
             builder1.setCancelable(false);
-            builder1.setPositiveButton("Çıkış Yap", new DialogInterface.OnClickListener() {
+            builder1.setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     mAuth.signOut();
-                    Intent intent = new Intent(Main5Activity.this, Main4Activity.class);
+                    Intent intent = new Intent(Main5Activity.this, Main4Activity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    finish();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 }
             });
-            builder1.setNegativeButton("Geri", new DialogInterface.OnClickListener() {
+            builder1.setNegativeButton("Back", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                 }
             });
             builder1.show();
 
+        }
+        if(models.get(position).getTitle()=="User Agreement"){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://nefisss.net/sozlesme.html"));
+            startActivity(intent);
+        }
+        if(models.get(position).getTitle()=="Developers"){
+            Intent intent = new Intent(Main5Activity.this, Main6Activity.class);//Geliştiriciler
+            startActivity(intent);
+        }
+        if(models.get(position).getTitle()=="Contact"){
+            Intent intent = new Intent(Main5Activity.this, Main7Activity.class);//İletişim
+            startActivity(intent);
         }
         //Object o = this.getListAdapter().getItem(position);
         //String pen = o.toString();
@@ -89,6 +105,6 @@ public class Main5Activity extends ListActivity {
     public void onBackPressed() {
         Intent intent = new Intent(Main5Activity.this, Main4Activity.class);
         startActivity(intent);
-        finish();
+        this.finish();
     }
 }
